7c61e2ff3c0948757a5ec87c604aa503829fae49
Tobias Kiertscher <dev@mastersign.de>
2015-11-13 16:21:45 +0100

    updated test case

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var fs = require('fs');
+var os = require('os');
 var test = require('tape').test;
 
 var mddata = require('../src/index');
@@ -8,10 +9,10 @@ var basePath = path.resolve('tests/data');
 
 var checkFileTransformation = function (t, fileName) {
 	var expectedFile = path.join(basePath, fileName + '.json');
-	var expected = JSON.parse(fs.readFileSync(expectedFile, 'utf-8'));
+	var expected = fs.readFileSync(expectedFile, 'utf-8').replace(new RegExp(os.EOL, "g"), "\n");
 
 	var sourceFile = path.join(basePath, fileName + '.md');
-	var result = mddata(sourceFile);
+	var result = JSON.stringify(mddata(sourceFile), null, '  ');
 	t.equals(expected, result, 'result after data extraction matches expected file content');
 	t.end();
 };
